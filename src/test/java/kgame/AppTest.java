a2f8354209113299021ca3e7aaca4678461480e0
TheKamboy <yaluigi10@gmail.com>
2023-05-26 16:53:19 -0700

    everything

---
+++ src/test/java/kgame/AppTest.java
@@ -0,0 +1,20 @@
+package kgame;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
