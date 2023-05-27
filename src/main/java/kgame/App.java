127d4e60698e7ef25bc025f9492564f7e4876ea1
TheKamboy <yaluigi10@gmail.com>
2023-05-26 18:00:23 -0700

    things

--- src/main/java/kgame/App.java
+++ src/main/java/kgame/App.java
@@ -1,13 +1,37 @@
 package kgame;
 
-/**
- * Hello world!
- *
- */
-public class App 
+// IO shit
+import java.io.IOException;
+
+// Terminal shit
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World" );
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+        terminal.clearScreen();
+
+        try {
+            TextGraphics tg = screen.newTextGraphics();
+
+            screen.startScreen();
+            screen.clear();
+
+            tg.putString(10, 10, "hi");
+            screen.refresh();
+
+            screen.readInput();
+        }
+        finally {
+            screen.stopScreen();
+            screen.close();
+        }
     }
 }
