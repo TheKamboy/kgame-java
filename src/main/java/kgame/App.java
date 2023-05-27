1ff8bb1920fb45f7c91a9500305de92b4a365667
TheKamboy <yaluigi10@gmail.com>
2023-05-26 18:53:43 -0700

    h

--- src/main/java/kgame/App.java
+++ src/main/java/kgame/App.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 // Terminal shit
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -12,26 +13,51 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 public class App
 {
+    public int kx  = 1;
+    public int ky  = 1;
+    public int kbx = kx;
+    public int kby = ky;
+
     public static void main( String[] args ) throws IOException
     {
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
-        Screen screen = new TerminalScreen(terminal);
-        terminal.clearScreen();
+        // Screen Clear
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        App app = new App();
 
         try {
-            TextGraphics tg = screen.newTextGraphics();
-
-            screen.startScreen();
-            screen.clear();
-
-            tg.putString(10, 10, "hi");
-            screen.refresh();
-
-            screen.readInput();
+            app.test();
         }
         finally {
-            screen.stopScreen();
-            screen.close();
+            System.exit(0);
+        }
+    }
+
+    public void test() throws IOException {
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+        TextGraphics tg = terminal.newTextGraphics();
+        terminal.setCursorVisible(false);
+
+        String hudtext = "";
+
+        while (true) {
+            screen.clear();
+
+            tg.putString(kx, ky, "hkrkekrkr");
+            tg.putString(10, 20, hudtext);
+
+            screen.refresh();
+
+            KeyStroke input = screen.readInput();
+
+            if (input != null) {
+                hudtext = String.format("Character Info: %s", input.toString());
+                if (input.getCharacter() == 'w') {
+
+                }
+            }
         }
     }
 }
